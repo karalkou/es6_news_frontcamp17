2c3f6d21c1b9d5211dc62205c3ddc75afa5e9bcc
Yury Karalkou <yury.karalkou@gmail.com>
2018-01-23 17:49:14 +0300

    [master] [YK] Remove old markup from 'NewsListItem'

--- src/components/news/NewsListItem.js
+++ src/components/news/NewsListItem.js
@@ -86,29 +86,6 @@ class NewsListItem extends Component {
                 __button={{ props: { href: url } }}
             />
         )
-
-       /* return (
-            <li className="news-list-item">
-                <div className="news-list-item__img" style={{ backgroundImage: `url(${urlToImage || customImageUrl})`}}/>
-
-                <div className="news-list-item__wrapper">
-                    <div className="news-list-item__header">
-                        <div className="news-list-item__date">
-                            ${formattedPublishedAt}
-                        </div>
-                    </div>
-                    <div className="news-list-item__data">
-                        <div className="news-list-item__content">
-                            <span className="news-list-item__author">${author}</span>
-                            <h1 className="news-list-item__title"><a href="#">${title}</a></h1>
-                            <p className="news-list-item__description">${description}</p>
-                            <a href={url} className="news-list-item__button">Read more</a>
-                        </div>
-                    </div>
-                </div>
-
-            </li>
-        )*/
     }
 }
 
